501af144077a1ee2642819c7a8af57b04ca88eef
nettools.ovh <dev@nettools.ovh>
2024-03-31 17:09:32 +0200

    test fix

--- tests/MailingEngine/MailingTest.php
+++ tests/MailingEngine/MailingTest.php
@@ -5,7 +5,7 @@ namespace Nettools\MassMailing\MailingEngine\Tests;
 
 
 use \Nettools\Mailing\Mailer;
-use \Nettools\MassMailing\MailingEngine\Mailing;
+use \Nettools\MassMailing\MailingEngine\Engine as MailingEngine;
 use \Nettools\MassMailing\TemplateEngine\Engine;
 use \org\bovigo\vfs\vfsStream;
 use \org\bovigo\vfs\vfsStreamDirectory;
@@ -26,10 +26,10 @@ class MailingTest extends \PHPUnit\Framework\TestCase
 		$mail = (new Engine())->template()->text('dummy content')->noAlternatePart()->build();
 		
 		// send mail
-		$m = (new Mailing($ml))
-				->from('unit-test@php.com')
-				->about('test subject')
-				->send($mail, 'recipient@domain.at');
+		$m = (new MailingEngine($ml))
+				->mailing([ 'from' => 'unit-test@php.com' ])
+					->about('test subject')
+					->send($mail, 'recipient@domain.at');
 
 		$sent = $ml->getMailerEngine()->getMailSender()->getSent();
 		$this->assertCount(1, $sent);	
